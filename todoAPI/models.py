5e929a6a3f8b9e59de527dd099bd16078c33baa7
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-17 15:49:43 +0300

    TODO Django API Documentation

--- todoAPI/models.py
+++ todoAPI/models.py
@@ -7,6 +7,7 @@ class Todo(models.Model):
     is_completed=models.BooleanField(default=False)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.title
+        
