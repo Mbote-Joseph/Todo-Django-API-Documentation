a98154474f4d0b5f866089fab3c68526c6272bd6
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-17 14:27:27 +0300

    TODO Django API Documentation

--- Todo/settings.py
+++ Todo/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # Apps
     'todoAPI',
     'rest_framework',
+    'coreapi',
     'drf_yasg',
 ]
 
--- Todo/urls.py
+++ Todo/urls.py
@@ -15,8 +15,34 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework import views, response
+from drf_yasg import openapi
+from rest_framework import permissions
+from rest_framework.documentation import include_docs_urls
+from drf_yasg.views import get_schema_view
+
+
+# Swagger documentation setup
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todoAPI.urls')),
+    path('api/v1/todo', include('todoAPI.urls')),
+    path('docs/', include_docs_urls(title='Todo Api')),
+    path('swagger/swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
