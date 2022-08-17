a52f21cfcf833eef235dc7c85e52495169736098
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-17 13:54:32 +0300

    TODO Django API Documentation

--- Todo/settings.py
+++ Todo/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps
+    'todoAPI',
+    'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- todoAPI/models.py
+++ todoAPI/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title=models.CharField(max_length=100)
+    body=models.CharField(max_length=200)
+    is_completed=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
