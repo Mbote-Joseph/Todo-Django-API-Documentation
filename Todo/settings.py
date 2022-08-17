b9b7fb63bbcada67c99280f610b37176e043c11d
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-17 16:13:54 +0300

    TODO Django API Documentation

--- Todo/settings.py
+++ Todo/settings.py
@@ -85,6 +85,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
