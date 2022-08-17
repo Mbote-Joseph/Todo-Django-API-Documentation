225ca40ac26de19c568750bed83fa2086dd9dd14
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-17 14:02:16 +0300

    TODO Django API Documentation

--- todoAPI/views.py
+++ todoAPI/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from todoAPI.models import Todo
+from todoAPI.serializers import TodoSerializer
 
 # Create your views here.
+class ListTodoAPIView(ListAPIView):
+    # Lists all todos from the database
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class CreateTodoAPIView(CreateAPIView):
+    # Creates a new todo in the database
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class UpdateTodoAPIView(UpdateAPIView):
+    # Updates a todo in the database
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DeleteTodoAPIView(DestroyAPIView):
+    # Deletes a todo from the database
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
